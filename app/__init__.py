5d90bdaf73f27a507effe69d46a584a0ded2ed12
Miroslav Brabenec <brabemi@gmail.com>
2019-07-25 23:24:38 +0200

    fixed missing import

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+from app import routes, models
